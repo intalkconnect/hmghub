b3051f2fe7a8b18ad942641e0273a1f73a9a93d0
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-07-08 20:22:48 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // ← ESSENCIAL PARA VERCEL
   plugins: [react()],
-  css: {
-    modules: false,
-  },
 });
-
