1f8739496eb3efc6c0078e6d4564752c7ab88708
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-07-08 21:27:59 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   css: {
-    modules: false
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    modules: false,
   },
 });
